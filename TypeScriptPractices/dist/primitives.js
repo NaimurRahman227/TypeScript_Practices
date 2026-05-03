e826f3de25db4f9fe8e0eb1b3b50382b3313b7bf
NaimurRahman227 <nmrahman1652@gmail.com>
2026-05-04 01:04:35 +0600

    primitives

---
+++ TypeScriptPractices/dist/primitives.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = 'naimurrahman';
+userName = '1223';
+console.log(userName);
+let res = userName.includes('2');
+console.log(res);
+//# sourceMappingURL=primitives.js.map
---
+++ TypeScriptPractices/src/primitives.ts
@@ -0,0 +1,5 @@
+let userName: string = 'naimurrahman';
+userName = '1223';
+console.log(userName);
+let res = userName.includes('2');
+console.log(res);
