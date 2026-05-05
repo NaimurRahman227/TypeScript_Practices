ecc1157e62247ba82f8bcce3cd888acd96d666bd
NaimurRahman227 <nmrahman1652@gmail.com>
2026-05-05 17:42:16 +0600

    object

---
+++ TypeScriptPractices/dist/nonprimitive.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let sodai = ['alu', 'begun', 'peyaj', 'lau'];
+console.log(sodai);
+let mixedSodai = ['chini', 'dudh', 23, true, false];
+mixedSodai.push('asdjks');
+console.log(mixedSodai);
+let tup = ['naimur', 23, 'sadia'];
+const student = {
+    namee: 'naimur',
+    nam: 'rahman',
+    age: 25,
+};
+console.log(student.namee);
+//# sourceMappingURL=nonprimitive.js.map
---
+++ TypeScriptPractices/src/nonprimitive.ts
@@ -0,0 +1,18 @@
+let sodai = ['alu' , 'begun' , 'peyaj' , 'lau'];
+
+console.log(sodai);
+
+let mixedSodai = ['chini' , 'dudh' , 23 , true , false];
+
+mixedSodai.push('asdjks');
+
+console.log(mixedSodai);
+
+let tup:[string , number , string] = ['naimur' , 23 , 'sadia'];
+
+const student:{readonly namee:string , nam:string , age:number} = {
+    namee : 'naimur',
+    nam : 'rahman',
+    age : 25,
+};
+console.log(student.namee);
