6fb5d83a55148a41e33635be9f6b236ee1d69469
NaimurRahman227 <nmrahman1652@gmail.com>
2026-05-05 21:04:42 +0600

    typeAlias

---
+++ TypeScriptPractices/src/typeAlias.ts
@@ -0,0 +1,26 @@
+const user:{
+    id: number,
+    name :{
+        fName : string,
+        lName : string,
+    },
+    gender : 'male' | 'Female',
+    house :{
+        vill: string,
+        thana : string,
+    },
+
+} = {
+    id : 1001,
+    name:{
+        fName : 'dsjkhf',
+        lName : 'dsfsdf'
+    },
+    gender : 'Female',
+    house:{
+        vill : 'harinathpur',
+
+        thana: 'kaxipur'
+    }
+
+};
