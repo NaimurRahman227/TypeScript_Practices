93246c62c5df584fb3e090646fc95350ce4599c3
NaimurRahman227 <nmrahman1652@gmail.com>
2026-05-09 00:46:37 +0600

    keyof operator

--- TypeScriptPractices/src/constraint.ts
+++ TypeScriptPractices/src/constraint.ts
@@ -2,3 +2,46 @@
 const createArrayFromString = (value:string) => [value];
 
 const createArrayFromNumber = (value:number) => [value];
+
+const createArrayFromObj = (value: {
+    id:number;
+    name:string;
+}) => [value];
+
+const StringArray = createArrayFromString('kallu')
+console.log(StringArray);
+
+const genericFunction = <T>(value: T) =>{
+    return[value];
+}
+const genericTuple = <X , Y> (param1: X , param2: Y) =>[param1,param2];
+
+
+const tuple1 = genericTuple('jamil' , 24)
+console.log(tuple1)
+
+// enroll student to a course
+
+const addStudent = <T>(studentInfo : T) =>{
+    return {
+        course : 'Next Level',
+        ...studentInfo,
+    };
+};
+
+const sutdent1 = {
+    id: 1001,
+    nmae: 'ANimuer',
+    seriuos: true,
+}
+
+const result1 = addStudent(sutdent1)
+console.log(result1)
+
+// keyof operator constraint
+type pagol = {
+    sex : 'male' | 'female';
+    skin : 'Back' | 'white';
+    height : number;
+    vill : string;
+};
