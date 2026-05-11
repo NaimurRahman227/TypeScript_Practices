2bfb3129d9a352f29c19c80e326e90e2bf88c3d0
NaimurRahman227 <nmrahman1652@gmail.com>
2026-05-12 00:19:26 +0600

    conditionalTypes More Exploration

--- TypeScriptPractices/src/conditionalType.ts
+++ TypeScriptPractices/src/conditionalType.ts
@@ -10,3 +10,28 @@ type richPeople = {
 }
 
 type vehicleInfo <T> = T extends keyof richPeople ? true : false;
+
+// Mapped types
+const arrayOfNumber: number[] = [1,2,3,54,6,7,8] 
+const arrayOFString : string[] = ['2' ,'33' , '56']
+
+const mappedArray : string[]  = arrayOfNumber.map((num) => num.toString());
+
+console.log(mappedArray)
+
+type array = {
+    height : number;
+    width: string;
+}
+
+type commonType <T> = {
+    [key in keyof T ] : T[key];
+};
+
+const abalChoda : commonType<array> = {
+    height : 35,
+    width : '35',
+};
+
+console.log(abalChoda)
+
