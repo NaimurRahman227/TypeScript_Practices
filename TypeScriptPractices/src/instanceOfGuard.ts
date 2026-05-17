2c894a6a88f3192c4c94e8f6791ac0f274290ac9
NaimurRahman227 <nmrahman1652@gmail.com>
2026-05-17 18:18:38 +0600

    instances

--- TypeScriptPractices/src/instanceOfGuard.ts
+++ TypeScriptPractices/src/instanceOfGuard.ts
@@ -4,6 +4,10 @@ class Person {
     constructor(name: string){
         this.name = name;
     }
+
+    getSleep(numberOfhours : number){
+        console.log(`he sleeps ${numberOfhours} in a day`)
+    }
 }
 
 
@@ -17,5 +21,32 @@ class Student extends Person {
     }
 }
 
+class Teacher extends Person {
+    constructor(name : string){
+        super(name);
+    }
+
+    takeClass(numberOfHopurs: number){
+        console.log(`${this.name} sir conduct ${numberOfHopurs} hours of class`);
+    }
+}
+
+const getUserInfo = (user : Person) =>{
+    if (user instanceof Student){
+        user.doStudy(6)
+    }
+    else if (user instanceof Teacher){
+        user.takeClass(4);
+    } else {
+        user.getSleep(7)
+    }
+
+
+}
+const student2 = new Student('Namur rahman')
+const teacher1 = new Teacher('Wakfor Ali')
+
 const student1 = new Student('faruk')
 console.log(Student)
+
+getUserInfo(student2);
