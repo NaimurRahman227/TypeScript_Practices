68cee41722ff243d4011c6e9688e0be80399c60f
NaimurRahman227 <nmrahman1652@gmail.com>
2026-05-17 22:35:16 +0600

    getter and setter method

---
+++ TypeScriptPractices/src/getterSetter.ts
@@ -0,0 +1,29 @@
+// Getter And Setter Method
+
+class openAccount {
+    userId : number;
+    userName: string;
+    private userBalance : number;
+    pass: number;
+
+    constructor(userId : number , userName : string , userbalance: number , pass : number){
+        this.userBalance = userbalance;
+        this.userId = userId;
+        this.userName = userName;
+        
+        this.pass = pass;
+
+    }
+    getBalance(pass: number , name : string){
+        if(pass === this.pass && name === this.userName){
+            return this.userBalance;
+        } else {
+            console.log('Credentials Does not match')
+        }
+        
+    }
+
+}
+
+const harun = new openAccount(1001 , 'akkku' , 1200 , 123455)
+console.log(harun.getBalance(123455 , 'akkku'))
