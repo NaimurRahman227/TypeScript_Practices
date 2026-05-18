520800b9ed69af195d9fc2e376bf8ac38dd14d85
NaimurRahman227 <nmrahman1652@gmail.com>
2026-05-19 01:26:40 +0600

    Abstruction

--- TypeScriptPractices/src/abstruction.ts
+++ TypeScriptPractices/src/abstruction.ts
@@ -4,4 +4,12 @@ class person {
     gender : string ;
     height : number;
     age : number;
+
+    constructor(name: string , skin : string, gender: string, height : number, age : number){
+        this.age = age;
+        this.gender = gender;
+        this.height = height;
+        this.skin = skin;
+        this.name = name;
+    }
 }
